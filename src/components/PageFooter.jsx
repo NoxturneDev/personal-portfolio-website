10a45931774815d0e6c263e441e18fcb7f9d07bc
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-19 20:51:10 +0700

    line animation on next page footer

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { motion } from 'framer-motion'
+import gsap from 'gsap'
 
 function PageFooter() {
+    const line = useRef(null)
+
+    const animateLine = () => {
+        gsap.fromTo(line.current, {
+            width: '2%'
+        }, {
+            width: '100%',
+            delay: 1.2,
+            ease: 'power2.out'
+        })
+    }
+
+    useEffect(() => {
+        animateLine()
+    }, [])
+
     return (
         <motion.div
             className="page-footer"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ delay: 1, duration: 1 , ease: "easeInOut" }}
+            transition={{ delay: 1, duration: 1, ease: "easeInOut" }}
         >
             <h1 className='footer-link header'>
                 <a href='#' target="_blank">Do You need a website? Contact me here</a>
@@ -15,12 +32,14 @@ function PageFooter() {
             <div className="footer-link next-page">
                 <a href="#">About</a>
             </div>
-            <span style={{
-                width: '10%',
-                height: '10px'
-            }}
+            <span
+                style={{
+                    width: '10%',
+                    height: '10px',
+                    textAlign: 'left'
+                }}
             >
-                <hr className='horizontal-line' />
+                <hr className='horizontal-line' ref={line} />
             </span>
         </motion.div>
     )
