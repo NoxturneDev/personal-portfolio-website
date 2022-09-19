00c12f8125a5e68663a2ec0eecc252472eaf7502
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-19 21:48:47 +0700

    add prop color to icon component

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export function NavIcon() {
+export function NavIcon({color = "#161616"}) {
     return (
         <svg width="24" height="9" viewBox="0 0 24 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0.903992 1.07999H23.0707M0.903992 8.37036H23.0707" stroke="#161616" stroke-linecap="round" />
+            <path d="M0.903992 1.07999H23.0707M0.903992 8.37036H23.0707" stroke={color} strokeLinecap="round" />
         </svg>
     )
 }
