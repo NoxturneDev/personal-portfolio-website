65ce39a0ad03fde15af0db0c808f2e6ce8d7920d
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-19 17:03:34 +0700

    create new footer component's style

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PageFooter() {
     return (
         <div className="page-footer">
-            <h1 className='footer-link'>
+            <h1 className='footer-link header'>
                 <a href='#' target="_blank">Do You need a website? Contact me here</a>
             </h1>
             <div className="footer-link next-page">
